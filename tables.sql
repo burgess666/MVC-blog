11f5464cda47732368f3e6f757953128018821d7
burgess666 <d14122793@mydit.ie>
2016-04-22 12:47:32 +0100

    bugs fixed with tables

--- tables.sql
+++ tables.sql
@@ -4,34 +4,32 @@ USE blog;
 
 CREATE TABLE b_comment
 (
-comment_id varchar(20),
-commented_date date,
-content varchar(50),
+comment_id varchar(20) NOT NULL,
+commented_date date NOT NULL,
+content varchar(50) NOT NULL,
 PRIMARY KEY (comment_id)
 );
 
 CREATE TABLE b_post
 (
-post_id varchar(20),
-comment_id varchar(20),
-posted_date date,
-title varchar(20) NOT NULL,
-content varchar(50),
+post_id varchar(20) NOT NULL,
+comment_id varchar(20) NOT NULL,
+posted_date date NOT NULL,
+title varchar(20) NOT NULL ,
+content varchar(50) NOT NULL,
 PRIMARY KEY (post_id),
 FOREIGN KEY (comment_id) REFERENCES b_comment(comment_id)
 );
 
 CREATE TABLE b_user(
 user_id varchar(20) NOT NULL,
-post_id varchar(20),
-comment_id varchar(20),
+post_id varchar(20) NOT NULL,
+comment_id varchar(20) NOT NULL,
 username varchar(20) NOT NULL,
-email varchar(50),
-passwd varchar(20),
+email varchar(50) NOT NULL,
+passwd varchar(20) NOT NULL,
 
 PRIMARY KEY (user_id),
 FOREIGN KEY (post_id) REFERENCES b_post(post_id),
 FOREIGN KEY (comment_id) REFERENCES b_comment(comment_id)
 );
-
-
